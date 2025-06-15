f1b084bd517c69f8c139f8f7f62195fe500b623e
Thedongraphix <chrisoketchcn@gmail.com>
2025-06-16 01:34:09 +0300

    New fix

--- api/index.ts
+++
@@ -1,5 +0,0 @@
-import { handleApiRequest } from '../index.js';
-
-// This function is the entry point for a serverless environment.
-// It simply passes the request and response objects to the main handler.
-export default handleApiRequest; 
